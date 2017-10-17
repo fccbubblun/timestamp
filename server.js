56b27f924705ee6661f8ebad972289bd430b098b
fccbubblun <forrestwilliams7c4@gmail.com>
2017-10-17 14:25:10 -0700

    added route to handle query strings

--- server.js
+++ server.js
@@ -3,9 +3,10 @@
 
 // init project
 var express = require('express');
+var moment = require('moment')
 var app = express();
 
-// we've started you off with Express, 
+// we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
@@ -13,7 +14,25 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + '/views/description.html');
+});
+
+app.get("/:date", function (request, response) {
+  var json = {"unix": null, "natural": null};
+  var date = moment.unix(Number(request.params.date));
+  console.log(date);
+  if(date.isValid()){
+    json.unix = date.unix();
+    json.natural = date.format("dddd, MMMM Do YYYY");
+    //json.natural = date.getFullYear();
+  } else {
+    date = moment(request.params.date);
+    if(date.isValid()){
+      json.unix = date.unix();
+      json.natural = date.format("dddd, MMMM Do YYYY");
+    }
+  }
+  response.send(json);
 });
 
 app.get("/dreams", function (request, response) {
